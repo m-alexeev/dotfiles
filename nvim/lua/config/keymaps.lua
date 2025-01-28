8f6779f86a847b6d1e827e2c820cbef559c3416d
Mikhail Alexeev <mishalxv35@gmail.com>
2025-01-28 10:48:02 -0800

    fixed config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
+vim.keymap.set("n", "<leader>rp", function()
+  local filepath = vim.fn.expand("%:p") -- Get the full path of the current file
+  local term = require("toggleterm.terminal").get(1) -- Get the first terminal
+  if term then
+    term:send("python3 " .. filepath, false) -- Send the command to the terminal
+  else
+    vim.notify("No active terminal found!", vim.log.levels.WARN)
+  end
+end, { desc = "Run Python in toggleterm" })
+
+vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        direction = "horizontal", -- Terminal will open in a horizontal split
+        size = 15, -- Height of the terminal when using horizontal split
+      })
+    end,
+  },
+}
