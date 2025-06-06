a5a2edb0070d0fb2ad832b43578a6a977a02868e
Mikhail Alexeev <mishalxv35@gmail.com>
2025-06-06 13:24:02 -0700

    Zed config

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    local conform = require("conform")
+
+    -- Specify the new formatter for jsonnet
+    opts.formatters = vim.tbl_extend("force", opts.formatters or {}, {
+      jsonnetfmt = {
+        command = "jsonnetfmt",
+        args = { "-" }, -- Override args to use stdin
+        stdin = true,
+      },
+    })
+
+    -- Specify which formatter to use for jsonnet filetype
+    opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
+      jsonnet = { "jsonnetfmt" },
+    })
+  end,
+}
