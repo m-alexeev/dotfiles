daf3e707aa5379a01670cc3fffc0f31b5d04c37b
m-alexeev <mishalxv35@gmail.com>
2025-07-19 20:30:46 -0700

    Wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- Visuals
+config.color_scheme = "Everforest Dark Medium (Gogh)"
+
+config.window_decorations = "RESIZE"
+
+config.initial_cols = 160
+config.initial_rows = 48
+config.window_background_opacity = 0.7
+config.macos_window_background_blur = 20
+
+return config
