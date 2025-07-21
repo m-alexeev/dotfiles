67b54d3818e784efcfa0f784179a32a830dd77dd
m-alexeev <mishalxv35@gmail.com>
2025-07-20 18:03:22 -0700

    Updated wez config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,4 +12,20 @@ config.initial_rows = 48
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 30
 
+-- Keybinds
+config.keys = {
+	{
+		key = "/",
+		mods = "CTRL",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 20 },
+		}),
+	},
+	{ key = "h", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Down") },
+}
+
 return config
