e0746f0c250ee71023b5cf3acbe44df7b57c6ea6
m-alexeev <mishalxv35@gmail.com>
2025-07-20 18:51:06 -0700

    Config updates to toggle bottom pane

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,20 +12,36 @@ config.initial_rows = 48
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 30
 
+wezterm.on("toggle_bottom_zoom_pane", function(window, pane)
+	local panes = window:active_tab():panes()
+	if #panes == 1 then
+		window:perform_action(
+			wezterm.action.SplitPane({
+				direction = "Down",
+				size = { Percent = 20 },
+			}),
+			pane
+		)
+	else
+		window:perform_action(wezterm.action.ActivatePaneByIndex(0), pane)
+		wezterm.time.call_after(0.05, function()
+			local new_pane = window:active_pane()
+			window:perform_action(wezterm.action.TogglePaneZoomState, new_pane)
+		end)
+	end
+end)
+
 -- Keybinds
 config.keys = {
+	{ key = "h", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "k", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "j", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Down") },
 	{
 		key = "/",
 		mods = "CTRL",
-		action = wezterm.action.SplitPane({
-			direction = "Down",
-			size = { Percent = 20 },
-		}),
+		action = wezterm.action.EmitEvent("toggle_bottom_zoom_pane"),
 	},
-	{ key = "h", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ key = "l", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Right") },
-	{ key = "k", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ key = "j", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Down") },
 }
 
 return config
