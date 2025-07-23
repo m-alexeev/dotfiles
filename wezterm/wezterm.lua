4e653658eff5cd63c68740f06bdee673f2b0669b
Mikhail Alexeev <mishalxv35@gmail.com>
2025-07-23 08:51:20 -0700

    reworked pane config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,38 +9,38 @@ config.window_decorations = "RESIZE"
 
 config.initial_cols = 160
 config.initial_rows = 48
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 30
-
-wezterm.on("toggle_bottom_zoom_pane", function(window, pane)
-	local panes = window:active_tab():panes()
-	if #panes == 1 then
-		window:perform_action(
-			wezterm.action.SplitPane({
-				direction = "Down",
-				size = { Percent = 20 },
-			}),
-			pane
-		)
-	else
-		window:perform_action(wezterm.action.ActivatePaneByIndex(0), pane)
-		wezterm.time.call_after(0.05, function()
-			local new_pane = window:active_pane()
-			window:perform_action(wezterm.action.TogglePaneZoomState, new_pane)
-		end)
-	end
-end)
 
 -- Keybinds
 config.keys = {
+	-- Move across windows
 	{ key = "h", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Left") },
 	{ key = "k", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Up") },
 	{ key = "l", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Right") },
 	{ key = "j", mods = "CTRL|CMD", action = wezterm.action.ActivatePaneDirection("Down") },
+	-- Toggle bottom term
 	{
 		key = "/",
 		mods = "CTRL",
-		action = wezterm.action.EmitEvent("toggle_bottom_zoom_pane"),
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	-- Split pane Vertically / Horizontally
+	{
+		key = "h",
+		mods = "SHIFT|CMD",
+		action = wezterm.action.SplitPane({ direction = "Left", size = { Percent = 50 } }),
+	},
+	{ key = "k", mods = "SHIFT|CMD", action = wezterm.action.SplitPane({ direction = "Up", size = { Percent = 50 } }) },
+	{
+		key = "l",
+		mods = "SHIFT|CMD",
+		action = wezterm.action.SplitPane({ direction = "Right", size = { Percent = 50 } }),
+	},
+	{
+		key = "h",
+		mods = "SHIFT|CMD",
+		action = wezterm.action.SplitPane({ direction = "Down", size = { Percent = 50 } }),
 	},
 }
 
