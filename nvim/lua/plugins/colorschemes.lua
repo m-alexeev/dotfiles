d310b5dcf2654a236dc8ec3a8b86283ef6bec0fa
Mikhail Alexeev <mikhail.alexeev@sanctuary.ai>
2026-07-06 09:56:13 -0700

    kanagawa

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,46 @@
 return {
   {
     "metalelf0/base16-black-metal-scheme",
-    lazy = false,
+    lazy = true,
     priority = 1000,
     config = function()
       vim.cmd.colorscheme("base16-black-metal-bathory")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      require("kanagawa").setup({
+        compile = true,
+        theme = "dragon",
+        background = { dark = "dragon", light = "lotus" },
+        colors = {
+          theme = {
+            dragon = {
+              ui = {
+                bg_visual = "#282727",  -- dragonBlack4
+                bg_search = "#393836",  -- dragonBlack5
+                pmenu = {
+                  fg = "#c5c9c5",       -- dragonWhite
+                  fg_sel = "none",
+                  bg = "#282727",       -- dragonBlack4
+                  bg_sel = "#393836",   -- dragonBlack5
+                  bg_thumb = "#625e5a", -- dragonBlack6
+                  bg_sbar = "#282727",  -- dragonBlack4
+                },
+                float = {
+                  fg_border = "#7a8382", -- dragonGray3
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+    config = function()
+      vim.cmd.colorscheme("kanagawa-dragon")
     end,
   },
   {
@@ -18,7 +54,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-black-metal-bathory",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
